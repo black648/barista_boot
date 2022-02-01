2113788fd0ea15f2ba7f95d3dc0cdbc745f35e94
HKL2 <bastlwalzer@gmail.com>
2022-02-01 22:32:14 +0900

    MemberDto 누락본 추가

---
+++ src/main/java/org/barista/service/member/dto/MemberDto.java
@@ -0,0 +1,39 @@
+package org.barista.service.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.barista.config.security.TokenProvider;
+import org.barista.service.member.entity.MemberEntity;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class MemberDto {
+
+    private String mberNo;
+    private String mberName;
+    private String mberId;
+    private String mberPhone;
+    private String address1;
+    private String address2;
+    private String mberSe;
+    private String email;
+    private String tokenKey;
+
+    public static MemberDto getMemberDto(MemberEntity entity, String tokenKey) {
+        return MemberDto.builder()
+                .mberNo(entity.getMberNo())
+                .mberName(entity.getMberName())
+                .mberId(entity.getMberId())
+                .mberPhone(entity.getMberPhone())
+                .address1(entity.getAddress1())
+                .address2(entity.getAddress2())
+                .mberSe(entity.getMberSe())
+                .email(entity.getEmail())
+                .tokenKey(tokenKey)
+                .build();
+    }
+}
